6bf76ebde662a6000a7db285cd2fb92b8b8c4cf3
sujith <sujikn73@gmail.com>
2024-06-25 23:34:53 +0400

    finding the prime number using anonymous function

---
+++ primearrow.js
@@ -0,0 +1,23 @@
+// **************arrow function************
+
+let n=34;
+let l=function (n)
+{
+   for(let i=2; i<=n; i++)
+   {
+      let flag=0;
+      for(let j=2; j<i; j++)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+      {
+         console.log(i);
+      }
+   }
+}
+l(n);
