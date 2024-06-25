5ddc32e8875929f1f3d7faca236b2d0222ce6298
sujith <sujikn73@gmail.com>
2024-06-25 23:36:47 +0400

    finding prime number using IIFE function

---
+++ primeiife.js
@@ -0,0 +1,21 @@
+// ******************IIFE************
+let n=34;
+(function (n)
+{
+   for(let i=2; i<=n; i++)
+   {
+      let flag=0;
+      for(let j=2; j<i; j++)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+      {
+         console.log(i);
+      }
+   }
+})(n);
