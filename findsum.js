45ef549930d325f9f278fe2d179f33b761923ce6
sujith <sujikn73@gmail.com>
2024-06-25 23:24:47 +0400

    finding the sum of array using anonymous function

---
+++ findsum.js
@@ -0,0 +1,12 @@
+
+// ************Anonymous***********
+var a=[1,2,3,4,5,6,7,8,9];
+var sum=0;
+n = function (a)
+{
+   for(let i=0; i<a.length; i++)
+   {
+     sum=sum+a[i];
+   }
+   return sum;
+}(a);
