8ec1659d565479373618733eef2a75d733b06ba0
sujith <sujikn73@gmail.com>
2024-06-26 00:07:33 +0400

    program for rorate array k times using anonymous function

---
+++ rotate.js
@@ -0,0 +1,23 @@
+// ******************anonymous*******************
+a = function (a, n, k)
+{
+    k = k % n;
+  
+    for (let i = 0; i < n; i++) {
+        if (i < k) {
+  
+           
+            console.log(a[n + i - k] + " ");
+        }
+        else {
+  
+            
+            console.log((a[i - k]) + " ");
+        }
+    }
+   
+}
+  let Array = [1, 2, 3, 4, 5];
+let N = Array.length;
+let K = 2;
+(Array, N, K);
