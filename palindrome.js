a4e9600a64545398b9e89a63f5a9d78289a68ccf
sujith <sujikn73@gmail.com>
2024-06-25 23:44:10 +0400

    finding palindrome anonymous function

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+// ****************Anonymous********
+  function (arr, n)
+    {
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }(arr,n)
