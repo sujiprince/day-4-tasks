f9e23a011b8034c536855651425e1658f671c8ef
sujith <sujikn73@gmail.com>
2024-06-25 22:42:40 +0400

    finding odd number using IIFE function

---
+++ findoddiife.js
@@ -0,0 +1,16 @@
+// ***********IIFE FUNCTION
+
+
+(function(array){
+
+    for(var i = 0 ; i< array.length ; i++){
+    
+    if(array[i]%2!=0){
+    
+    console.log(array[i])
+    
+    }
+    
+    }
+    
+    })([1,2,3,4])
