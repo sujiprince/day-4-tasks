4e9c4286c11575089144ee0c24df63604636677d
sujith <sujikn73@gmail.com>
2024-06-25 23:29:34 +0400

    find the sum of array using arrow function

---
+++ sumarrow.js
@@ -0,0 +1,14 @@
+// *********arrow function***********
+
+var a=[1,2,3,4,5,6,7,8,9];
+
+var sum=0;
+let arr=function (a)
+{
+   for(let i=0; i<a.length; i++)
+   {
+     sum=sum+a[i];
+   }
+   return sum;
+}
+arr(a);
